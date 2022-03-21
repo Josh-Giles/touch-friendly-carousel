036d5e39b1361737a70d3ae46bb1e97ee17f4622
JoshGiles <jodhgiles78@googlemail.com>
2022-03-21 15:14:51 +0000

    Now have scroll snapping
    I now have the slideshow snapping to the nearest element.
    
    This does need some refinement as the code is probably not as efficient or robust as it could be but this is a good start and I think it has the fundamentals

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
 var momentumID;
 var carousel;
 var imageLocations = [];
+var approxImageLocations = [];
 
 
 function momentumScroll(){
     carousel.scrollLeft += scrollVelocity;
     scrollVelocity *= 0.95;
-    if (!imageLocations.includes(carousel.scrollLeft)){    
-        if (Math.abs(scrollVelocity) > 0.5){       
-            momentumID = requestAnimationFrame(momentumScroll);
-        }
+
+    if (approxImageLocations.includes(Math.round(carousel.scrollLeft/100)*100)) {
+        scrollToNearest(carousel.scrollLeft);
+    }
+    else if (Math.abs(scrollVelocity) > 0.5){       
+        momentumID = requestAnimationFrame(momentumScroll);
+    }
+    else{
+        scrollToNearest(carousel.scrollLeft);
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+function scrollToNearest(currentScrollPosition){
+    const output = imageLocations.reduce((prev, curr) => Math.abs(curr - currentScrollPosition) < Math.abs(prev - currentScrollPosition) ? curr : prev);
+    carousel.scroll({top:0, left:output, behavior:'smooth'}); 
+}
 
-    for (let image of document.getElementsByClassName("carouselImage")) {
-        imageLocations.push(image.x);
-    }
+document.addEventListener("DOMContentLoaded", function() {
 
     carousel = document.querySelector(".carouselContainer")
     let startPosition;
     let scrollLeft;
     let carouselBeingClicked = false;
+
+    for (let image of document.getElementsByClassName("carouselImage")) {
+        imageLocations.push(image.offsetLeft);
+        approxImageLocations.push(Math.round(image.offsetLeft/100)*100)
+    }
 
     carousel.addEventListener("mousedown", function(event){
         carouselBeingClicked = true;
